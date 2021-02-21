321705a149124702e93289bfe3290570e34bd2df
azu <azuciao@gmail.com>
2021-02-21 15:08:19 +0900

    update

--- src/rss.ts
+++ src/rss.ts
@@ -7,7 +7,7 @@ import { RSSItem } from "./index";
 const BASE_URL = "https://azu.github.io/github-search-rss";
 export const BOOK_FEEDS: RSSItem[] = [
     {
-        title: "TypeScript Iteration Plan",
+        title: "microsoft/TypeScript Iteration Plan",
         query: "repo:microsoft/TypeScript is:issue label:Planning",
         TYPE: "ISSUE",
         link: `${BASE_URL}/typescript-iterator-plan.json`
