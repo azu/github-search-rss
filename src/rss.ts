04c2b9d614b2bd8ffbdd2d83bc8ba70936781d05
azu <azu@users.noreply.github.com>
2022-01-15 21:46:33 +0900

    Update rss.ts

--- src/rss.ts
+++ src/rss.ts
@@ -31,6 +31,12 @@ export const SEARCH_ITEMS: RSSItem[] = [
         TYPE: "ISSUE",
         link: `${BASE_URL}/npm-rfcs.json`
     },
+    {
+        title: "github/roadmap Issues",
+        query: "repo:github/roadmap is:issue",
+        TYPE: "ISSUE",
+        link: `${BASE_URL}/github-roadmap.json`
+    },
     // Pull Request
     {
         // label:data:
